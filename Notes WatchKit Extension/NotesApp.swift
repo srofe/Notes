81752a2ddbc9026b592088314d1dd2675e24bf01
Simon Rofe <simon.rofe@ieee.org>
2021-06-22 12:55:59 +1000

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Simon Rofe on 22/6/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
