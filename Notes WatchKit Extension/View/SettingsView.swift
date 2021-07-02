4b6fe6e1e302f7fed13cb5afac801d0acf18c96a
Simon Rofe <simon.rofe@ieee.org>
2021-07-02 13:34:56 +1000

    Add slider to settings view

--- Notes WatchKit Extension/View/SettingsView.swift
+++ Notes WatchKit Extension/View/SettingsView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct SettingsView: View {
     @AppStorage("lineCount") var lineCount: Int = 1
+    @State private var value: Float = 1.0
 
     var body: some View {
         VStack(spacing: 8) {
             HeaderView(title: "Settings")
             Text("Lines \(lineCount)".uppercased())
                 .fontWeight(.bold)
+            Slider(value: $value, in: 1 ... 4, step: 1)
+                .accentColor(.accentColor)
         }
     }
 }
