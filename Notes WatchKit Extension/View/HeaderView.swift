cb98ce0af11b8a60d6e836f88c33321222b46ee9
Simon Rofe <simon.rofe@ieee.org>
2021-07-02 13:00:49 +1000

    Add an optional title to the header view

--- Notes WatchKit Extension/View/HeaderView.swift
+++ Notes WatchKit Extension/View/HeaderView.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct HeaderView: View {
+    var title: String = ""
+
     var body: some View {
-        HStack {
-            Capsule()
-                .frame(height: 1)
-            Image(systemName: "note.text")
-            Capsule()
-                .frame(height: 1)
-        }
-        .foregroundColor(.accentColor)    }
+        VStack {
+            if title != "" {
+                Text(title.uppercased())
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.accentColor)
+            }
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+                Image(systemName: "note.text")
+                Capsule()
+                    .frame(height: 1)
+            }
+            .foregroundColor(.accentColor)
+        }    }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        Group {
+            HeaderView()
+            HeaderView(title: "Credits")
+        }
     }
 }
