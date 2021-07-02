cf4caa71495c760ba84afafb5d38c906d5294af5
Simon Rofe <simon.rofe@ieee.org>
2021-07-02 13:08:36 +1000

    Randomise profile image

--- Notes WatchKit Extension/View/CreditsView.swift
+++ Notes WatchKit Extension/View/CreditsView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct CreditsView: View {
+    @State private var randomNumber: Int = Int.random(in: 1 ..< 4)
+    private var randomImage: String {
+        return "developer-no\(randomNumber)"
+    }
+
     var body: some View {
         VStack(spacing: 3) {
-            Image("developer-no1")
+            Image(randomImage)
                 .resizable()
                 .scaledToFit()
                 .layoutPriority(1)
